672c0c80414c0f317383ec31127181598666a052
linearproblem <47994813+linearproblem@users.noreply.github.com>
2023-05-31 13:56:57 +1000

    Initial

---
+++ main.py
@@ -0,0 +1,33 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+    # Todo:
+    #   - Setup imports
+    #   - Define locations for files
+    #   - (menu will eventually go here)
+    #   - Get Settings (hardcode bottle for now)
+    #   - Setup Camera
+    #   - Setup camera links
+
+    # Standard library imports
+    import os
+    import sys
+
+    # Related third-party imports
+    import numpy as np
+
+    # Local imports
+    from my_package import my_module
+    from my_package.sub_package import my_other_module
